11bd871a4b0a14f66a66f944ef3afdae04928453
Lutvi Rosyady <moorails@gmail.com>
2014-12-12 20:18:18 +0700

    Fixing Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-if ENV["REDISCLOUD_URL"]
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
-  Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-	Redis.current = Redis.new(YAML.load_file("#{Rails.root}/config/redis.yml")["config"])
-end
+# if ENV["REDISCLOUD_URL"]
+#   uri = URI.parse(ENV["REDISCLOUD_URL"])
+#   Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# else
+# 	Redis.current = Redis.new(YAML.load_file("#{Rails.root}/config/redis.yml")["config"])
+# end
