57bef6fb58fec166249503e96a7d9f90f63957ac
Lutvi Rosyady <moorails@gmail.com>
2014-12-12 20:31:45 +0700

    Fixing Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
-if Rails.env.production?
-  uri = URI.parse("redis://rediscloud:m8EzKCcQEdBsNAMj@pub-redis-19586.us-east-1-2.2.ec2.garantiadata.com:19586")
-  Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV["REDISCLOUD_URL"]
+   Redis.current = Redis.new(:url => ENV["REDISCLOUD_URL"])
 else
 	Redis.current = Redis.new(YAML.load_file("#{Rails.root}/config/redis.yml")["config"])
 end
